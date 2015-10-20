33c3b3870ce638f7a34e2444c263860c4022896b
Marcel Eichner <love@ephigenia.de>
2015-10-20 08:49:26 +0200

    adds buttons for font size increase & decrease

--- source/scripts/App.js
+++ source/scripts/App.js
@@ -22,7 +22,34 @@ app.config(['$routeProvider', function($routeProvider) {
 
 }]);
 
-app.run(function($rootScope, $location, $http, $log) {
+app.run(function($rootScope, $location, $http, $log, Config) {
 
+  $rootScope.setFontSize = function(sizePixel) {
+    $log.info('Setting font size to %dpx', sizePixel);
+    var bodyElm = document.querySelector('body');
+    bodyElm.style.fontSize = sizePixel + "px";
+  };
+  $rootScope.increaseFontSize = function() {
+    if (!Config.fontSize) {
+      Config.fontSize = 12;
+    }
+    Config.fontSize += 1;
+    Config.save();
+    $log.info('Increase font size to ', Config.fontSize);
+    $rootScope.setFontSize(Config.fontSize);
+  };
+  $rootScope.decreaseFontSize = function() {
+    if (!Config.fontSize) {
+      Config.fontSize = 12;
+    }
+    Config.fontSize -= 1;
+    Config.save();
+    $log.info('Decrease font size to ', Config.fontSize);
+    $rootScope.setFontSize(Config.fontSize);
+  };
+
+  if (Config.fontSize) {
+    $rootScope.setFontSize(Config.fontSize);
+  }
 
 });
