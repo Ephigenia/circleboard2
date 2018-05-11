8f5afd9595558ec2461782afffc58e05ba9af5f2
Ephigenia <love@ephigenia.de>
2018-05-11 16:51:19 +0200

    test: fixes e2e tests and adds them to CI

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -7,8 +7,8 @@ describe('circleboard3 App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display the application name & version', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getPageTitle()).toEqual('Circleboard 3.0.0-alpha.1');
   });
 });
