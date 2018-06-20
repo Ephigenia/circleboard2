e1d79bafd2d8e83d5648f401a2fed32602a2ff8f
Ephigenia <love@ephigenia.de>
2018-06-20 12:16:57 +0200

    test: fixes tdd and test run

--- karma.conf.js
+++ karma.conf.js
@@ -47,7 +47,7 @@ module.exports = function (config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: false,
+    autoWatch: true,
     browsers: ['ChromeHeadless'],
     singleRun: true,
   });
